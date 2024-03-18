9c35934a22945a213974c3a518966b58f531b980
Muhammad Nur Hakimi Husni <hakimihusni97@gmail.com>
2024-03-18 23:22:37 +0800

    Add react-router-dom and configure main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <h1 className="text-3xl font-bold underline">
       Hello world!
@@ -13,4 +6,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
